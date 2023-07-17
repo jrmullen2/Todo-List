729036d2e47b70b918c0f21418016869d63c07c6
Jackson Mullen <jrmullen2@icloud.com>
2023-07-17 10:15:02 -0700

    Modify set functions so stack overflow error does not occur. Transfer contents from index.js

--- src/createTask.js
+++ src/createTask.js
@@ -1,57 +1,64 @@
 //A class that produces a task and its basic info
 export function createTodo() {
+  const userTitle = document.getElementById("userTitle");
+  const userDescrip = document.getElementById("userDescrip");
+  const userDue = document.getElementById("userDue");
+  const userPriority = document.getElementById("userPriority");
+  const userNotes = document.getElementById("userNotes");
   let titleInput = "";
   let descripInput;
   let dueInput;
   let priorityInput;
   let notesInput;
+
+  obtainInfo();
+
   class Task {
     constructor(title, descrip, due, priority, notes) {
-      this.title = title;
-      this.descrip = descrip;
-      this.due = due;
-      this.priority = priority;
-      this.notes = notes;
+      this._title = title;
+      this._descrip = descrip;
+      this._due = due;
+      this._priority = priority;
+      this._notes = notes;
     }
     set title(value) {
-      this.title = value;
+      this._title = value;
     }
     set descrip(value) {
-      this.descrip = value;
+      this._descrip = value;
     }
     set due(value) {
-      this.due = value;
+      this._due = value;
     }
     set priority(value) {
-      this.priority = value;
+      this._priority = value;
     }
     set notes(value) {
-      this.notes = value;
+      this._notes = value;
     }
     get title() {
-      return this.title;
+      return this._title;
     }
     get descrip() {
-      return this.descrip;
+      return this._descrip;
     }
     get due() {
-      return this.due;
+      return this._due;
     }
     get priority() {
-      return this.priority;
+      return this._priority;
     }
     get notes() {
-      return this.notes;
+      return this._notes;
     }
   }
   function obtainInfo() {
     titleInput = userTitle.value;
     descripInput = userDescrip.value;
     dueInput = userDue.value;
-    priorityInput = userNotes.value;
+    priorityInput = userPriority.value;
     notesInput = userNotes.value;
   }
-  obtainInfo();
   const newTask = new Task(
     titleInput,
     descripInput,
@@ -59,6 +66,12 @@ export function createTodo() {
     priorityInput,
     notesInput
   );
-  return newTask;
+  return {
+    newTask,
+    titleInput,
+    descripInput,
+    dueInput,
+    priorityInput,
+    notesInput,
+  };
 }
-//implement into index
