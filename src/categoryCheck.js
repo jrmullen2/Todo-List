2397f79a596d20b17e0f6d586627f63be5dd3129
Jackson Mullen <jrmullen2@icloud.com>
2023-07-27 22:03:48 -0700

    Return currentDate

--- src/categoryCheck.js
+++ src/categoryCheck.js
@@ -2,15 +2,13 @@
 export function categoryCheck() {
   const date = new Date();
   let currentDate = date.toISOString().slice(0, 10);
-  const userDue = document.getElementById("userDue");
+  const userDue = document.querySelector(".userDue");
 
   function dateMin() {
     userDue.setAttribute("min", currentDate);
     userDue.setAttribute("value", currentDate);
   }
   function sortDiv() {
-    console.log(userDue.value);
-    console.log(currentDate);
     if (userDue.value === currentDate) {
       return "today";
     } else if (userDue.value !== currentDate) {
@@ -35,6 +33,6 @@ export function categoryCheck() {
     });
   }
 
-  return { dateMin, sortDiv, moveOld };
+  return { dateMin, sortDiv, moveOld, currentDate };
 }
 // make left div clicks display. Currently an issue where classlist is undefined according to browser
